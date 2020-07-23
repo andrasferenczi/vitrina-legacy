0fd11ca0782bd339e8eb084fd2cd447ccc7ac215
Andras Ferenczi <a.ferenczi.andras@gmail.com>
2020-07-23 18:49:19 +0200

    BOOM

---
+++ app/src/main/java/stoyck/vitrina/MainActivity.kt
@@ -0,0 +1,18 @@
+package stoyck.vitrina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import stoyck.vitrina.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/stoyck/vitrina/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package stoyck.vitrina.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

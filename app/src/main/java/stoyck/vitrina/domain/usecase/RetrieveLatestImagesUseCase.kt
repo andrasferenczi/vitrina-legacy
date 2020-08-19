d107e6ea325aae3a9d443a973a38f157da2115fb
ariel martini <bortao@gmail.com>
2020-08-19 17:11:47 -0300

    Keep the latest images when refreshing

--- app/src/main/java/stoyck/vitrina/domain/usecase/RetrieveLatestImagesUseCase.kt
+++ app/src/main/java/stoyck/vitrina/domain/usecase/RetrieveLatestImagesUseCase.kt
@@ -16,20 +16,27 @@ class RetrieveLatestImagesUseCase @Inject constructor(
 ) {
 
     suspend operator fun invoke(): List<RedditPost> {
-        val previousPosts = loadPosts()
-        val existingPostIds = previousPosts.map { it.id }.toSet()
+        // val previousPosts = loadPosts()
+        // val existingPostIds = previousPosts.map { it.id }.toSet()
 
         val subreddits = loadSubreddits()
         val preferences = loadSettings()
 
         val subredditNames = subreddits.map { it.name }
 
+        val maxItems = 15 // Max number of images returned
+        val itemsPerSubreddit = maxItems / subreddits.size
+
         val posts = if (preferences.shuffle) {
             reddit.retrievePosts(subredditNames)
         } else {
-            subredditNames.flatMap {
+            subreddits.flatMap {
                 // Todo: rate limit these calls, or reddit will do it
-                reddit.retrievePosts(it)
+                reddit
+                    .retrievePosts(it.name)
+                    .filter { post -> it.minUpvoteCount < post.score && !post.stickied }
+                    .sortedByDescending { it.created } // sort before limiting
+                    .take (itemsPerSubreddit)
             }
         }
 
@@ -44,19 +51,19 @@ class RetrieveLatestImagesUseCase @Inject constructor(
             .asSequence()
             // for easy indexing
             .map { it.copy(subreddit = it.subreddit.toLowerCase(Locale.ROOT)) }
-            .filter { it.id !in existingPostIds }
             .filter {
                 val minUpvoteCount = minUpvoteCounts[it.subreddit] ?: return@filter false
                 return@filter minUpvoteCount < it.score
             }
             .filterImages()
+            .sortedByDescending { it.created }
             .toList()
             // Do not load too many images,
             // Might be too much load in network for a single time
-            .take(15)
+            .take (maxItems)
 
         // use map so that it stops at the breakpoint
-        return images // .map { it }
+        return images.map { it }
     }
 
     private fun Sequence<RedditPost>.filterImages(): Sequence<RedditPost> {

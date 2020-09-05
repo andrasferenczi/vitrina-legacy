657306fadac27fff92d3f13624266c3decc44eef
Andras Ferenczi <a.ferenczi.andras@gmail.com>
2020-09-05 09:25:25 +0200

    Implemented adding new images without overwriting the entire queue in Muzei

--- app/src/main/java/stoyck/vitrina/muzei/VitrinaArtWorker.kt
+++ app/src/main/java/stoyck/vitrina/muzei/VitrinaArtWorker.kt
@@ -4,12 +4,13 @@ import android.content.Context
 import android.util.Log
 import androidx.work.*
 import com.google.android.apps.muzei.api.provider.ProviderClient
-import com.google.android.apps.muzei.api.provider.ProviderContract
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.withContext
 import stoyck.vitrina.BuildConfig
 import stoyck.vitrina.VitrinaApplication
 import stoyck.vitrina.domain.usecase.TryRetrieveNextMuzeiArtworkUseCase
+import stoyck.vitrina.muzei.ext.pruneOldArtworks
+import stoyck.vitrina.muzei.ext.retrieveVitrinaProviderClient
 import java.util.concurrent.Executors
 import javax.inject.Inject
 
@@ -50,24 +51,13 @@ class VitrinaArtWorker(
             .inject(this)
     }
 
-//    private suspend fun tryRetrieveNextArtwork(): Pair<Artwork, File>? {
-//        val singleImage = tryRetrieveNextMuzeiArtwork()
-//            ?: return null
-//
-//        return singleImage.
-//    }
-
-    private suspend fun updatePictures(
+    private suspend fun loadNewArtworks(
         provider: ProviderClient,
-        newPictureCount: Int,
-        extraImagesInMuzei: Int
+        newPictureCount: Int
     ) {
-        val totalImages = newPictureCount + extraImagesInMuzei
-
         for (i in 0 until newPictureCount) {
             val (artwork, file) = tryRetrieveNextMuzeiArtwork() ?: break
 
-            // todo: remove last
             provider.addArtwork(artwork)?.also {
                 applicationContext.contentResolver.openOutputStream(it)?.use { output ->
                     file.inputStream().use { input ->
@@ -83,32 +73,10 @@ class VitrinaArtWorker(
             Log.i(TAG, "Started doing work")
         }
 
-        val provider = ProviderContract
-            .getProviderClient(
-                applicationContext,
-                BuildConfig.VITRINA_AUTHORITY
-            )
+        val provider = applicationContext.retrieveVitrinaProviderClient()
 
-        updatePictures(provider, 3, 10)
-
-//        // keep the last one as first when setting
-//        val latest = provider.lastAddedArtwork
-//
-//        val prev = provider.readArtworks()
-//
-//        val newArtworks =
-//            if (latest == null)
-//                artworks
-//            else
-//                listOf(latest, *artworks.toTypedArray())
-//
-////        provider.addArtwork(listOf()).map {
-////            provider.context.contentResolver.openOutputStream(it).use {
-////
-////            }
-////        }
-//
-//        provider.setArtwork(newArtworks)
+        loadNewArtworks(provider, 4)
+        provider.pruneOldArtworks(applicationContext, 16)
 
         return@withContext Result.success()
     }
--- app/src/main/java/stoyck/vitrina/muzei/ext/providerClient.kt
+++ app/src/main/java/stoyck/vitrina/muzei/ext/providerClient.kt
@@ -1,8 +1,22 @@
 package stoyck.vitrina.muzei.ext
 
+import android.content.ContentProviderOperation
 import android.content.Context
+import android.content.OperationApplicationException
+import android.os.RemoteException
+import android.util.Log
 import com.google.android.apps.muzei.api.provider.Artwork
 import com.google.android.apps.muzei.api.provider.ProviderClient
+import com.google.android.apps.muzei.api.provider.ProviderContract
+import stoyck.vitrina.BuildConfig
+
+fun Context.retrieveVitrinaProviderClient(): ProviderClient {
+    return ProviderContract
+        .getProviderClient(
+            applicationContext,
+            BuildConfig.VITRINA_AUTHORITY
+        )
+}
 
 fun ProviderClient.readArtworks(context: Context): List<Artwork> {
     return context.contentResolver
@@ -10,7 +24,7 @@ fun ProviderClient.readArtworks(context: Context): List<Artwork> {
         .use { cursor ->
 
             if (cursor == null) {
-                return@use emptyList()
+                return@use emptyList<Artwork>()
             }
 
             val result = mutableListOf<Artwork>()
@@ -25,4 +39,45 @@ fun ProviderClient.readArtworks(context: Context): List<Artwork> {
 
             return@use result
         }
+        .sortedByDescending { it.dateAdded }
+}
+
+fun ProviderClient.removeArtworks(context: Context, vararg artworks: Artwork) {
+    val contentResolver = context.contentResolver
+
+    val operations = artworks
+        .map { artwork ->
+            ContentProviderOperation.newDelete(contentUri)
+                .withSelection(
+                    "${ProviderContract.Artwork._ID} = ?",
+                    arrayOf(artwork.id.toString())
+                ).build()
+        }
+
+
+    try {
+        val results =
+            contentResolver.applyBatch(BuildConfig.VITRINA_AUTHORITY, ArrayList(operations))
+
+        if (BuildConfig.DEBUG) {
+            results.forEach {
+                Log.v("VitrinaProviderClient", it.toString())
+            }
+        }
+    } catch (ignored: OperationApplicationException) {
+    } catch (ignored: RemoteException) {
+    }
+}
+
+fun ProviderClient.pruneOldArtworks(context: Context, maxCount: Int) {
+    val artworks = readArtworks(context)
+
+    val artworkCountToRemove = artworks.size - maxCount
+
+    if (artworkCountToRemove <= 0) {
+        return
+    }
+
+    val idsToRemove = artworks.takeLast(artworkCountToRemove)
+    removeArtworks(context, *idsToRemove.toTypedArray())
 }
